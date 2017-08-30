6850d879e5f9e5075a67f08dd278fab090da0dce
Miguelort095 <31443985+Miguelort095@users.noreply.github.com>
2017-08-30 04:30:48 -0400

    add Files

--- functions.php
+++ functions.php
@@ -16,15 +16,26 @@
 	add_theme_support('post-thumbnails');
 	set_post_thumbnail_size( 150, 150, true );
 
-	// Permitir comentarios encadenados
 
-	function enable_threaded_comments()
-	{
-		if (is_singular() AND comments_open() AND (get_option('thread_comments' ) == 1)) {
-			wp_enqueue_script('comment-reply');
-		}
-		
-	}
-	add_action('get_header','enable_threaded_comments');
+	// Main sidebar 
+	if(function_exists('register_sidebar)')) // Funcion para iniciar el Sidebar
+		register_sidebar( array(
+			'name' => 'main Sidebar', // Nombre del Sidebar
+			'before_widget' => '<hr>'; // Salto de linea
+			'after_widget' => '',
+			'before_title' => '<h3>', // Apertura de la etiqueta h3
+			'after_title' => '</h3>' // Cierre de la etiqueta h3
+			));
 
+//Para Habilitar thumbnails
+add_theme_support('post-thumbnails');
+set_post_thumbnail_size(150, 150, true);
+
+
+ // Function para comentarios encadenados
+function enable_threaded_comments(){
+    if (is_singular() AND comments_open() AND (get_option('thread_comments') == 1)) {
+       wp_enqueue_script('comment-reply');
+    }
+} add_action('get_header', 'enable_threaded_comments'); 
  ?>
--- sidebar.php
+++ sidebar.php
@@ -1,4 +1,13 @@
-<form method="get" id="searchform" action="<?php bloginfo('url'); ?>"/>
-	<input type="text" placeholder="Buscar.." value="<?php the_search_query(); ?>" name="s" id="s" />
-		<button type="submit" class="icon-only" id="searchsubmit"></button>
+<form method="get" id="searchform" action="<?php bloginfo('url'); ?>/">
+  <input type="text" placeholder="Buscar…" value="<?php the_search_query(); ?>" name="s" id="s" />
+  <button type="submit" class="icon-only"  id="searchsubmit"></button>
 </form>
+
+
+<section id="sidebar"> <!-- Inicio de section, para llamar functons de Sidebar -->
+	<?php 
+	if(!function_exists('dynamic_sidebar') || // Si la funcion no existe !! o es diferente 
+		!dynamic_sidebar( 'main Sidebar')) : // O es diferente va a imprimir los valores llamados entre ''
+	 ?>
+	<?php endif ?> <!-- Cierre del If -->
+</section>
