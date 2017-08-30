ff910843de24a0f04714c292c91f897099aea27f
SamuelCampo <31445815+SamuelCampo@users.noreply.github.com>
2017-08-30 03:43:18 -0430

    Actualización de Footer

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 		<footer>
-			<p>&amp;&amp;&amp;copy; <?php bloginfo('name'); ?>, <?php echo date('Y') ?>. Inversiones Prohost 2021</p>
+			<p>&amp;&amp;&amp;copy; <?php bloginfo('name'); ?>, <?php echo date('Y') ?>. Desarrollador por Inversiones Prohost 2021</p>
 		</footer>
 		</div> <!-- Fin del Wrapper --> 
 	</body>
